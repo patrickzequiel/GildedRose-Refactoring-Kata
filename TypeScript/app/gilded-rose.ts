a96a21ac38c4f063ad449dfbcc8c89c0984e69b5
PCZCJ55 <patrick.cruz@inetum.com>
2026-06-21 12:20:29 +0200

    using variable names instead of comparing multiple times the names

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,61 +18,66 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name === 'Conjured Mana Cake') {
-              this.items[i].quality = Math.max(0, this.items[i].quality - 2);
-            } else {
-              this.items[i].quality = this.items[i].quality - 1
+    this.items.forEach((item) => {
+      const isSulfuras = item.name === 'Sulfuras, Hand of Ragnaros'
+      const isAgedBrie = item.name === 'Aged Brie';
+      const isBackstage = item.name === 'Backstage passes to a TAFKAL80ETC concert'
+      const isConjured = item.name === 'Conjured Mana Cake';
+        if (!isAgedBrie && !isBackstage) {
+          if (item.quality > 0) {
+            if (!isSulfuras) {
+              if (isConjured) {
+                item.quality = Math.max(0, item.quality - 2);
+              } else {
+                item.quality = item.quality - 1
 
-            }
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
               }
             }
           }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                if (this.items[i].name === 'Conjured Mana Cake') {
-                  this.items[i].quality = Math.max(0, this.items[i].quality - 2);
-                } else {
-                  this.items[i].quality = this.items[i].quality - 1
-
+        } else {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
+            if (isBackstage) {
+              if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1
+                }
+              }
+              if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1
                 }
               }
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
           }
         }
-      }
-    }
+        if (!isSulfuras) {
+          item.sellIn = item.sellIn - 1;
+        }
+        if (item.sellIn < 0) {
+          if (!isAgedBrie) {
+            if (!isBackstage) {
+              if (item.quality > 0) {
+                if (!isSulfuras) {
+                  if (isConjured) {
+                    item.quality = Math.max(0, item.quality - 2);
+                  } else {
+                    item.quality = item.quality - 1
+
+                  }
+                }
+              }
+            } else {
+              item.quality = item.quality - item.quality
+            }
+          } else {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+
+    })
 
     return this.items;
   }
