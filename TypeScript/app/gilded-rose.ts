093a7cb2b9de2d3a0e8392ba3d25861be8d7db5b
PCZCJ55 <patrick.cruz@inetum.com>
2026-06-21 12:10:07 +0200

    fixed business logic to pass all the unit tests to start refactoring

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -22,7 +22,12 @@ export class GildedRose {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+            if (this.items[i].name === 'Conjured Mana Cake') {
+              this.items[i].quality = Math.max(0, this.items[i].quality - 2);
+            } else {
+              this.items[i].quality = this.items[i].quality - 1
+
+            }
           }
         }
       } else {
@@ -50,7 +55,12 @@ export class GildedRose {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+                if (this.items[i].name === 'Conjured Mana Cake') {
+                  this.items[i].quality = Math.max(0, this.items[i].quality - 2);
+                } else {
+                  this.items[i].quality = this.items[i].quality - 1
+
+                }
               }
             }
           } else {
